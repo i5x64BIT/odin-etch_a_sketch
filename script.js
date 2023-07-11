d8abde6ef44f68bec7e805039d61253f3be2028a
i5x64BIT <vaintomer@gmail.com>
2023-07-11 15:49:14 +0300

    Add board resize, remove dynamic resize
    The flexible dynamic resize causes big a performance hit.
    A flexible and responsive board will be possible with a grid template.
    Dynamics can be solved with media querries, making a responsive grid
    (slider with no apply button) with calculations is a waste of time.

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
 let board = document.querySelector('.board');
 let pageWidth = document.documentElement.clientWidth;
 let pageHeight = document.documentElement.clientHeight;
+let slider = document.getElementById('board-size-slider');
+let btnApply = document.getElementById('display-btn-apply');
 
 let rowSize = 16;
 let boardSize = rowSize**2;
 
-board.style.height = board.clientWidth + 'px';
 
-//Board fill
-for(let i = 0; i < boardSize; i++){ 
-    pixel = document.createElement("div");
-    pixel.classList.add('pixel')
-    pixel.style.width = board.clientWidth/rowSize + 'px';
-    pixel.style.height = board.clientHeight/rowSize + 'px';
-    board.appendChild(pixel);
+const drawPixels = () => {
+    board.innerHTML = ''; //remove childern
+
+    for(let i = 0; i < boardSize; i++){ 
+        pixel = document.createElement("div");
+        pixel.classList.add('pixel')
+        pixel.style.width = board.clientWidth/rowSize + 'px';
+        pixel.style.height = board.clientHeight/rowSize + 'px';
+        board.appendChild(pixel);
+    }
+    console.log
 }
-
-window.addEventListener("resize", () =>{
-    board.style.height = board.clientWidth + 'px';
-    const pixels = document.getElementsByClassName('pixel')
-
+const resizePixels = (pixels) =>{
     for(p of pixels){
         p.style.width = board.clientWidth/rowSize + 'px';
         p.style.height = board.clientHeight/rowSize + 'px';
     }
+}
+
+drawPixels();
+
+//Slider functionality
+slider.oninput = function(){
+    rowSize = this.value;
+    document.getElementById('display-size').innerText = `${rowSize}x${rowSize}`;
+}
+
+btnApply.addEventListener('click', () => {
+    boardSize = rowSize**2; //update board size
+    drawPixels();
 })
+
+
+
+// //On resize
+// window.addEventListener("resize", () =>{
+//     board.style.height = board.clientWidth + 'px';
+//     const pixels = document.getElementsByClassName('pixel')
+
+//     resizePixels(pixels);
+// })
+
