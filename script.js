d2c29a0c2ba5a935112bdf397e9e23aa58a75990
i5x64BIT <vaintomer@gmail.com>
2023-07-12 18:26:16 +0300

    Add drawing And clear board
    CSS was adjusted to the extra button.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let pageWidth = document.documentElement.clientWidth;
 let pageHeight = document.documentElement.clientHeight;
 let slider = document.getElementById('board-size-slider');
 let btnApply = document.getElementById('display-btn-apply');
+let btnClear = document.getElementById('display-btn-clear')
 
 let rowSize = 16;
 let boardSize = rowSize**2;
@@ -10,15 +11,24 @@ let boardSize = rowSize**2;
 
 const drawPixels = () => {
     board.innerHTML = ''; //remove childern
+    let isMouseActive = false;
+
 
     for(let i = 0; i < boardSize; i++){ 
         pixel = document.createElement("div");
         pixel.classList.add('pixel')
         pixel.style.width = board.clientWidth/rowSize + 'px';
         pixel.style.height = board.clientHeight/rowSize + 'px';
+
+        pixel.addEventListener('mousedown', () => { isMouseActive = true; })
+        pixel.addEventListener('mouseover', function(e){
+            if(isMouseActive){
+                this.style.backgroundColor = "black";
+            }
+        })
+        pixel.addEventListener('mouseup', () =>{ isMouseActive = false; })
         board.appendChild(pixel);
     }
-    console.log
 }
 const resizePixels = (pixels) =>{
     for(p of pixels){
@@ -26,11 +36,17 @@ const resizePixels = (pixels) =>{
         p.style.height = board.clientHeight/rowSize + 'px';
     }
 }
+const clearBoard = () => {
+    document.querySelectorAll('.pixel').forEach((p) => {
+        p.style.backgroundColor = 'white';
+    })
+}
 
 drawPixels();
 
-//Slider functionality
-slider.oninput = function(){
+//Controls
+
+slider.oninput = function(){  //Slider Functionality
     rowSize = this.value;
     document.getElementById('display-size').innerText = `${rowSize}x${rowSize}`;
 }
@@ -39,6 +55,8 @@ btnApply.addEventListener('click', () => {
     boardSize = rowSize**2; //update board size
     drawPixels();
 })
+
+btnClear.addEventListener('click', clearBoard);
 
 
 
